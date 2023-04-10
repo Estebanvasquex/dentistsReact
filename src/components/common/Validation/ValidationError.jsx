93bd3e7b1a3da149b3640e4d8ba9c5984e2036a1
estebanvasquex <estebanvasquex@gmail.com>
2023-04-09 21:59:40 -0500

    validación material

--- src/components/common/Validation/ValidationError.jsx
+++ src/components/common/Validation/ValidationError.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
 
 const ValidationError = () => {
   return (
-    <div>
-        Debe Validar los datos ingresados
-    </div>
+    <Stack sx={{ width: '100%' }} spacing={2}>
+    <Alert severity="error">Debe Validar los datos ingresados!</Alert>
+
+  </Stack>
+
   )
 }
 
--- src/components/common/Validation/ValidationOk.jsx
+++ src/components/common/Validation/ValidationOk.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+
 const ValidationOk = () => {
   return (
-    <div>Sus datos fueron guardados correctamente, mil gracias</div>
+    <Stack sx={{ width: '100%' }} spacing={2}>
+    <Alert severity="success">Sus datos fueron guardados correctamente, mil gracias!</Alert>
+  </Stack>
+   
   )
 }
 
